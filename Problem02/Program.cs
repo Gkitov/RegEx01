a86a33b4662544f3c34d1928c7373de153ec0c9c
Galin <Galin@DESKTOP-CAL3NOT>
2024-03-12 16:25:29 +0200

    Add project files.

---
+++ Problem02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+      
+        string text = Console.ReadLine();
+
+        string pattern = @"\+359([\ \-])2\1\d{3}\1\d{4}\b";
+
+        Regex regex = new Regex(pattern);
+
+        MatchCollection matches = regex.Matches(text);
+
+       
+
+        foreach (Match match in matches)
+        {
+            Console.Write(match.Value + ", ");
+        }
+    }
+}
---
+++ RegEx01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string text = Console.ReadLine();
+
+        string pattern = @"\b[A-Z][a-z]{1,}[ ]{1}[A-Z][a-z]{1,}\b";
+
+        Regex regex = new Regex(pattern);
+
+        MatchCollection matches = regex.Matches(text);
+
+        List<string> result = new List<string>();
+
+        foreach (Match match in matches)
+        {
+            result.Add(match.Value);
+            
+        }
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
